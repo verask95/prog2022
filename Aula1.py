2e867f32efa521bce1fbf6ac96a4996d6c35adbd
verask95 <115596965+verask95@users.noreply.github.com>
2022-10-24 21:05:25 -0300

    Update Aula1.py

---
+++ Aula1.py
@@ -0,0 +1,27 @@
+A = 5
+B = 6
+C = 3
+D = "Charles"
+E = "Cesar"
+F = "charles"
+
+
+X = (A > B)
+
+print (X)
+
+Y = (B <= C)
+
+print (Y)
+
+Z = (D == E)
+
+print (Z)
+
+W = (D == F)
+
+print (W)
+
+T = (D > E)
+
+print (T)
