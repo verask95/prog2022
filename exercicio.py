5e77d52df2da8330290a009ec586e8db1ce30820
verask95 <115596965+verask95@users.noreply.github.com>
2022-10-25 00:20:45 -0300

    Update exercicio.py

--- exercicio.py
+++ exercicio.py
@@ -1,19 +1,18 @@
-###DEFININDO VALORES###
-A = float(input('VALOR DE A: '))
-B = float(input('VALOR DE B: '))
+A = float(input('VALOR DE A:'))
+B = float(input('VALOR DE B:'))
 
-NOME = str(input('NOME: '))
+NOME = str(input('NOME:'))
+TESTE = False
+PROF = str(input('PROFISSÃO:'))
 
-PROF = str(input('PROFISSÃO: '))
-
-###OPERANDO AS VARIAVEIS###
+###REALIZANDO AS OPERAÇÕES###
 a = (A + 1 >= (B ** (1/2))) or (NOME != 'ANA')
 b = (A + 1 >= (B ** (1/2))) and (PROF == 'MEDICO')
 c = (NOME != 'ANA') or (PROF == 'MEDICO') and (A + 1 >= (B ** (1/2)))
-d = not 'TESTE' and (( A + 1 ) >= (B ** (1/2))) or not (PROF == 'MEDICO')
-e = not ((A + 1 >= (B ** (1/2))) and 'TESTE')
+d = not TESTE and (( A + 1 ) >= (B ** (1/2))) or not (PROF == 'MEDICO')
+e = not ((A + 1 >= (B ** (1/2))) and TESTE)
 
-###IMPRIMINDO OS RESULTADOS###
+###IMPRIMINDO RESULTADOS###
 print (a)
 print (b)
 print (c)
