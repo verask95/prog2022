18758ba886971eb8647ebe7eb17bb2bbb879f52d
verask95 <115596965+verask95@users.noreply.github.com>
2022-11-01 21:32:55 -0300

    Update Aula3.py

--- Aula3.py
+++ Aula3.py
@@ -1,6 +1,10 @@
-nome = input('informe seu nome:')
+##calculo de notas
 
-print(f'O Nome Digitado foi;{nome}')
+n1 = int (input ('Informe a primeira nota:'))
 
+n2 = int (input ('Informe a segunda nota:'))
 
+media = (n1 *2 + n2 *3) /5
+
+print(f'A média é {media}')
 
