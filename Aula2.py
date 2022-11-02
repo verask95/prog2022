2431451ebac75160bd1823cb9ffd4afea0fd538d
verask95 <115596965+verask95@users.noreply.github.com>
2022-11-01 21:11:48 -0300

    Update Aula2.py

--- Aula2.py
+++ Aula2.py
@@ -4,12 +4,12 @@ raio = input('Informe o raio:')
 
 altura = input('Informe a altura:')
 
-raio = int(raio)
+raio = float(raio)
 
-altura = int(altura)
+altura = float(altura)
 
 ##Calcule volume
 
-volume = 3.141619 * raio**2 * altura
+volume = 3.1416 * raio**2 * altura
 
 print(f'O volume do cilindro de raio {raio} e {altura} é {volume}')
