91be6bd5ce273f69c184099825d3eba1e7356e54
verask95 <115596965+verask95@users.noreply.github.com>
2022-11-01 21:47:01 -0300

    Update Aula3.py

--- Aula3.py
+++ Aula3.py
@@ -1,8 +1,8 @@
 ##calculo de média ponderada
 
-n1 = int (input ('Informe a sua primeira nota:'))
+n1 = int(input('Informe a sua primeira nota:'))
 
-n2 = int (input ('Informe a sua segunda nota:'))
+n2 = int(input('Informe a sua segunda nota:'))
 
 media = (n1 *2 + n2 *3) /5
 
