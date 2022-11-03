a9edd817aeae395fd0e9af19731505215305e72c
verask95 <115596965+verask95@users.noreply.github.com>
2022-11-03 19:36:30 -0300

    Update Aula4.py

---
+++ Aula4.py
@@ -0,0 +1,8 @@
+n1 = int(input('Digite um número inteiro:'))
+
+if n1 == 0:
+    print("Este numero é nulo!")
+elif n1%2 == 0:
+    print("Este numero é par")
+else:
+    print("Este numero é impar")
